980ced65609eb9f7f0075a120f68efc71cfd83be
Tyson910 <Tyson910@lve.missouristate.edu>
2020-10-05 19:24:42 -0500

    delete unneccesary files

--- src/index1.js
+++ src/index1.js
@@ -3,8 +3,6 @@
 
   import Welcome from '/lib/Welcome.js'
   import Clock from '/lib/clock.js'
-  import listy from '/lib/listy.js';
-  import activityData from '/lib/activityData.js';
   import CitySelector from '/lib/citySelector.js';
   import PickActivity from '/lib/activitySelector.js';
   
