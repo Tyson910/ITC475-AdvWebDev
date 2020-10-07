ce4d3dae8e8b32255a32950520291079c4c116dd
Tyson910 <Tyson910@lve.missouristate.edu>
2020-10-07 14:37:05 -0500

    create function to store keys/values for activitySelector

--- src/activitySelector.js
+++ src/activitySelector.js
@@ -1,59 +1,49 @@
+import activityList from '/lib/activityList.js'
 class PickActivity extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            activities: '',
+            outings: '',
+            chosenOutings: ''
         };
     }
 
 
     componentDidUpdate(prevProps){
-
-        const maldivesData = ['Beach', 'Sailing', 'Muesems', 'Hiking', 'Boating']
-        const cancunData = ['Beaches', 'Boating', 'Parks and Recreation', 'Snorkeling'];
-        const veniceData = ['Museums', 'Theatre', 'Parks and Recreation', 'City Tours'];
-        const NZData = ['City Tours', 'Sports', 'Cycling', 'Museums', 'Boating'];
-
-
+        
         if (this.props.city !== prevProps.city){
+
             this.setState({city: this.props.city});
 
-            if (this.props.city == 'Maldives'){
-                this.setState({activities:maldivesData});
-            }
-            else if (this.props.city == 'Cancun'){
-                this.setState({activities:cancunData});
-            }
-            else if (this.props.city == 'New Zealand'){
-                this.setState({activities:NZData});
-            }
-            else if (this.props.city == 'Venice'){
-                this.setState({activities:veniceData});
-            }
+            this.setState({outings: activityList(this.props.city)});
+            
+            const listy = activityList(this.props.city);
+            const listyArray = Object.values(listy);
+            console.log( listyArray.map( item => (item.activity)));
             
     }
     
     }
 
     //<input type="submit" value="submit" />
-    
+
     render(){
-        const outingsArray = Object.values(this.state.activities) ; 
+        const outingsArray = Object.values(this.state.outings) ; 
+        const showArray = outingsArray.map(item =>(
+            <div key={item.id} >
+            <input type="checkbox" name='Outing' value={item.activity}></input>
+            <label htmlFor={item.activity}>{item.activity}</label>
+            <br></br>
+            </div>
+            ));
 
         return (
+        
             <form className='outingList'>
-            <h3>Activities</h3>
-            
-            {outingsArray.map(item =>(
-                <div>
-                <input type="checkbox" key={item} id={item} name={item} value={item}></input>
-                <label htmlFor={item}>{item}</label>
-                <br></br>
-                </div>
-                ))}
-                
- 
-                
+
+            <h3>Activty</h3>
+
+            <div>{showArray}</div>    
 
             </form>
         );
